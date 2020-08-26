487eb49765cb59d93810e673fd1a4d1f235a0c66
Alexandre Hamez <alexandre.hamez@gmail.com>
2020-08-26 22:24:23 +0200

    More status codes

--- apps/xomium/lib/xomium/google/http_status_error.ex
+++ apps/xomium/lib/xomium/google/http_status_error.ex
@@ -3,23 +3,35 @@ defmodule Xomium.Google.HttpStatus do
   https://developers.google.com/drive/api/v3/handle-errors
   """
 
-  def status(400, json) do
-    {:bad_request, reason(json)}
+  def reason(400, json) do
+    {:bad_request, reason_text_to_atom(json)}
   end
 
-  def status(403, json) do
-    {:forbidden, reason(json)}
+  def reason(401, json) do
+    {:forbidden, reason_text_to_atom(json)}
   end
 
-  def status(404, "Not Found") do
+  def reason(403, json) do
+    {:forbidden, reason_text_to_atom(json)}
+  end
+
+  def reason(404, "Not Found") do
     {:not_found, "Not Found"}
   end
 
-  def status(429, json) do
-    {:too_many_requests, reason(json)}
+  def reason(429, json) do
+    {:too_many_requests, reason_text_to_atom(json)}
   end
 
-  defp reason(json) do
+  def reason(500, json) do
+    {:internal_server_error, reason_text_to_atom(json)}
+  end
+
+  def reason(status_code, json) do
+    {status_code, reason_text_to_atom(json)}
+  end
+
+  defp reason_text_to_atom(json) do
     [%{"reason" => reason}] = json["error"]["errors"]
 
     case reason do
@@ -28,6 +40,9 @@ defmodule Xomium.Google.HttpStatus do
       "userRateLimitExceeded" -> :user_rate_limit_exceeded
       "rateLimitExceeded" -> :rate_limit_exceeded
       "invalidParameter" -> :invalid_parameter
+      "authError" -> :auth_error
+      "internalError" -> :internal_error
+      _ -> :unknown
     end
   end
 end
